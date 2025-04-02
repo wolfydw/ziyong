39d985a741df17e69d7742893028bca6607beebf
wolfydw <wolfydw@gmail.com>
2025-04-02 14:40:02 +0800

    Update auto-update.sh

--- compose/auto-update.sh
+++ compose/auto-update.sh
@@ -6,11 +6,11 @@ SCRIPT_PATH="./docker-auto-update.sh"
 # 2. 创建docker-auto-update.sh脚本
 echo "Creating docker-auto-update.sh script..."
 
-cat > "$SCRIPT_PATH" <<EOL
+cat > "$SCRIPT_PATH" <<'EOL'
 #!/bin/bash
 # docker compose一键更新脚本
-# 通过输入docker-auto-update实现一键更新
-# 执行docker compose down, pull 和 up
+# 通过输入 docker-auto-update 实现一键更新
+# 执行 docker compose down, pull 和 up
 docker compose down && docker compose pull && docker compose up -d
 EOL
 
@@ -18,9 +18,10 @@ EOL
 chmod +x "$SCRIPT_PATH"
 echo "docker-auto-update.sh script created and made executable."
 
-# 4. 添加别名到~/.bashrc
+# 4. 确保脚本路径正确，并添加 alias 到 ~/.bashrc
+ABSOLUTE_PATH="$(pwd)/docker-auto-update.sh"
 echo "Setting up alias for docker-auto-update..."
-echo "alias docker-auto-update='$(pwd)/docker-auto-update.sh'" >> ~/.bashrc
+echo "alias docker-auto-update='$ABSOLUTE_PATH'" >> ~/.bashrc
 
 # 5. 使更改生效
 source ~/.bashrc
